a7d0766eb5f027eca64973cac7c7f981c484f01b
Felype Naiff <felypenaiff01@gmail.com>
2026-03-15 00:48:28 +0000

    Try fixing this error: `Runtime ReferenceError: Select is not defined. E

--- src/app/(dashboard)/processos/grupos/page.tsx
+++ src/app/(dashboard)/processos/grupos/page.tsx
@@ -17,6 +17,13 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { 
+  Select, 
+  SelectContent, 
+  SelectItem, 
+  SelectTrigger, 
+  SelectValue 
+} from "@/components/ui/select"
 import { toast } from "@/hooks/use-toast"
 import { cn } from "@/lib/utils"
 import { 
